95b9f708caea254b27c98377135dc152e1bf34a2
Kgiruc <kgiruc76@gmail.com>
2022-05-10 17:32:15 +0200

    React-dom conf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './scss/main.scss'
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>elo</h1>
+<>
+    <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+    </Routes>
 
-    </div>
+</>
   );
 }
 

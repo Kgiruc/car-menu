c18f681db4b8ee9c6e433fa96f46d278f0dd7d34
Kgiruc <kgiruc76@gmail.com>
2022-05-11 16:19:13 +0200

    add clock

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,58 +1,13 @@
 import React, {useState} from "react";
+import WeatherApp from "../widgets/Weather-app";
+import Clockdata from "../widgets/clockdata";
+
 
 export default function Home() {
-
-    const apiKey = '2c53647046dd58ea69b179191f13dbb2';
-    const [weatherData, setWeatherData] = useState([{}])
-    const [city, setCity] = useState("")
-
-
-
-    const getWeather = (event) => {
-        if(event.key === 'Enter'){
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`)
-                .then(
-                    response => response.json()
-                ).then(
-                    data => {
-                        setWeatherData(data);
-                        setCity("")
-
-                    }
-            )
-        }
-    }
-
-
-
-    return (
-        <div className="weather-container">
-            <input className="weather-box" placeholder="Enter City"
-                    onChange={e => setCity(e.target.value)}
-                    value={city}
-                    onKeyPress={getWeather}
-            />
-
-
-            {typeof weatherData.main === 'undefined' ? (
-                <div>
-                    <p>Enter ur localisation to get the weather</p>
-                </div>
-
-            ): (
-                <div className="weather-data">
-                    <p>{weatherData.name}</p>
-                    <p>{Math.round((weatherData.main.temp -32)*5/9)}°C</p>
-                    <p>{weatherData.weather[0].main}</p>
-                </div>
-            )}
-
-            {weatherData.cod === "404" ? (
-                <p>City not found</p>
-            ):(
-                <>
-                </>
-            )}
-        </div>
+return(
+    <>
+        <WeatherApp/>
+        <Clockdata/>
+    </>
     )
 }

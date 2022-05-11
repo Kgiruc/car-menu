d2bc75c96e1aea952d106dea19874bb046f659d2
Kgiruc <kgiruc76@gmail.com>
2022-05-11 15:21:30 +0200

    weather-data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './scss/main.scss'
 import './App.css';
 import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
+import Navigation from "./pages/Navigation";
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
 <>
     <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
+        <Route path='/about' element={<Navigation/>} />
     </Routes>
 
 </>
